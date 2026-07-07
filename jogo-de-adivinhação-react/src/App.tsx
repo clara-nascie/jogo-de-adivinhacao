c7283434d3ba69ac801e03aa0c7c4d844ec99488
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-07 16:39:16 -0300

    style: wrap letters in word container and style main layout

--- jogo-de-adivinhação-react/src/App.tsx
+++ jogo-de-adivinhação-react/src/App.tsx
@@ -14,11 +14,13 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Uma das linguagens de programação mais populares do mundo"/>
+        <div className={styles.word}>
         <Letter value="r"/>
         <Letter value="e"/>
         <Letter value="a"/>
         <Letter value="c"/>
         <Letter value="t"/>
+        </div>
       </main>
     </div>
   );
