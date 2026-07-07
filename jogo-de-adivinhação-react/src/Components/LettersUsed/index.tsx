00f538cde761dea6d8c7d2a7e8478cdd1e2b9096
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-07 18:17:55 -0300

    feat: support small size option in Letter and display used letters

--- jogo-de-adivinhação-react/src/Components/LettersUsed/index.tsx
+++ jogo-de-adivinhação-react/src/Components/LettersUsed/index.tsx
@@ -1,10 +1,14 @@
-import styles from './styles.module.css';
-
+import styles from "./styles.module.css";
+import { Letter } from "../Letter";
 
 export function LettersUsed() {
-    return (
-        <div className={styles.lettersUsed}>
-            <h5>Letras utilizadas</h5>
-        </div>
-    );
+  return (
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+      <div>
+        <Letter value="x" size="small" />
+        <Letter value="r" size="small" />
+      </div>
+    </div>
+  );
 }
